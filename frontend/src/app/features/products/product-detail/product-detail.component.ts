940e4030cbf943607aacbb11298ecaa6195a4a93
Ioana - Alexandra Tituleasa <ioanaalexandra.tituleasa@gmail.com>
2026-05-11 20:56:29 +0300

    in progress product details page

---
+++ frontend/src/app/features/products/product-detail/product-detail.component.ts
@@ -0,0 +1,51 @@
+import { Component, OnInit, signal } from '@angular/core';
+import { ActivatedRoute, RouterModule } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { ProductService } from '../../../core/services/product.services';
+import { Product } from '../../../core/models/product.models';
+
+@Component({
+  selector: 'app-product-detail',
+  standalone: true,
+  imports: [CommonModule, RouterModule],
+  templateUrl: './product-details.component.html',
+  styleUrl: './product-detail.component.css'
+})
+export class ProductDetailComponent implements OnInit {
+  product = signal<Product | null>(null);
+  loading = signal(true);
+  error = signal<string | null>(null);
+
+  constructor(
+    private route: ActivatedRoute,
+    private productService: ProductService
+  ) {}
+
+  ngOnInit(): void {
+    const id = Number(this.route.snapshot.paramMap.get('id'));
+    this.productService.getProductById(id).subscribe({
+      next: (data) => {
+        this.product.set(data);
+        this.loading.set(false);
+      },
+      error: () => {
+        this.error.set('Product not found or server error.');
+        this.loading.set(false);
+      }
+    });
+  }
+
+  get stockLabel(): string {
+    const stock = this.product()?.stock ?? 0;
+    if (stock === 0) return 'Out of Stock';
+    if (stock < 5) return `Only ${stock} left!`;
+    return `In Stock (${stock})`;
+  }
+
+  get stockClass(): string {
+    const stock = this.product()?.stock ?? 0;
+    if (stock === 0) return 'text-danger';
+    if (stock < 5) return 'text-warning';
+    return 'text-success';
+  }
+}
