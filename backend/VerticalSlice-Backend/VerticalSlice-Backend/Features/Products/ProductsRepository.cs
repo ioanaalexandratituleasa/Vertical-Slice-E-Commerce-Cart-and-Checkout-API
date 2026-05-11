dfcb2c10ea12ca5549e37276c7108e4111f25085
Ioana - Alexandra Tituleasa <ioanaalexandra.tituleasa@gmail.com>
2026-05-11 22:13:08 +0300

    done with get all products

--- backend/VerticalSlice-Backend/VerticalSlice-Backend/Features/Products/ProductsRepository.cs
+++ backend/VerticalSlice-Backend/VerticalSlice-Backend/Features/Products/ProductsRepository.cs
@@ -53,5 +53,41 @@ namespace VerticalSlice_Backend.Features.Products
             return null;
         }
 
+        public async Task<IEnumerable<ProductDetailDTO>> GetAllProductsAsync()
+        {
+            var products = new List<ProductDetailDTO>();
+            using var connection = _dbConnectionFactory.CreateConnection();
+            connection.Open(); 
+
+            const string sql = @"SELECT ProductID, Title, DescriptionP, Price, ImageP, Stock 
+                         FROM Products";
+
+            using var command = connection.CreateCommand();
+            command.CommandText = sql;
+
+            using var reader = await ((SqlCommand)command).ExecuteReaderAsync();
+
+            while (await reader.ReadAsync())
+            {
+                products.Add(new ProductDetailDTO
+                {
+                    ProductID = reader.GetInt32(reader.GetOrdinal("ProductID")),
+                    Title = reader.GetString(reader.GetOrdinal("Title")),
+                    DescriptionP = reader.IsDBNull(reader.GetOrdinal("DescriptionP"))
+                                ? string.Empty
+                                : reader.GetString(reader.GetOrdinal("DescriptionP")),
+                    Price = reader.GetDouble(reader.GetOrdinal("Price")),
+                    ImageP = reader.IsDBNull(reader.GetOrdinal("ImageP"))
+                                ? string.Empty
+                                : reader.GetString(reader.GetOrdinal("ImageP")),
+                    Stock = reader.IsDBNull(reader.GetOrdinal("Stock"))
+                             ? 0
+                             : reader.GetInt32(reader.GetOrdinal("Stock"))
+                });
+            }
+
+            return products;
+        }
+
     }
 }
