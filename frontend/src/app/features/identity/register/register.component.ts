3f2924e9ec05c534c297c5be257dfbf56c43ba2e
Ioana - Alexandra Tituleasa <ioanaalexandra.tituleasa@gmail.com>
2026-05-14 17:24:29 +0300

    fixing register page

--- frontend/src/app/features/identity/register/register.component.ts
+++ frontend/src/app/features/identity/register/register.component.ts
@@ -38,13 +38,24 @@ export class RegisterComponent{
     }).subscribe({
       next: (res) => {
         this.success.set(`Cont creat cu succes! Bine ai venit, ${res.fName}!`);
-        this.loading.set(false);
-        setTimeout(() => this.router.navigate(['/products']), 2000);
-      },
-      error: (err) => {
-        this.error.set(err.error || 'A apărut o eroare.');
-        this.loading.set(false);
-      }
-    });
+        this.authService.login({
+        email: this.email,
+        password: this.password
+      }).subscribe({
+        next: () => {
+          this.loading.set(false);
+          setTimeout(() => this.router.navigate(['/products']), 2000);
+        },
+        error: () => {
+          this.loading.set(false);
+          setTimeout(() => this.router.navigate(['/login']), 2000);
+        }
+      });
+    },
+    error: (err) => {
+      this.error.set(err.error || 'A apărut o eroare.');
+      this.loading.set(false);
+    }
+  });
   }
 }
